b9f07a98d27625dd03ca05d5fdd179a7a94bf6b1
dyvandong <584791857@qq.com>
2019-09-16 19:04:43 +0800

    change

--- gradesign/gradesign/settings.py
+++ gradesign/gradesign/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'gradesign.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gradesign',
+        'NAME':'maoyan',
+        'HOST':'152.136.104.104',
         'USER':'root',
-        'PASSWORD':'dzd123',
-        'PORT':'3306',
-        'HOST':'localhost',
+        'PASSWORD':'dyvan1013',
+        'PORT':3306,
+        'CHARSET':'utf8'
     },
 }
 
@@ -108,15 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- gradesign/mydesign/urls.py
+++ gradesign/mydesign/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
+    url(r'^$', views.index),
     url(r'^single/(\d+)/$', views.single),
     url(r'^comment/(\d+)/$', views.comment),
     url(r'^like/$', views.like),
